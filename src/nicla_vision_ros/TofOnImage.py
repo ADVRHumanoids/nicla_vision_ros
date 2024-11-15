ff1a884880b0cd6acb083f504969b5fc82493e36
Davide Torielli <toridebraus@gmail.com>
2024-11-15 17:23:16 +0100

    method so more confortable to use from external

--- src/nicla_vision_ros/TofOnImage.py
+++ src/nicla_vision_ros/TofOnImage.py
@@ -1,10 +1,39 @@
 #!/usr/bin/env python3
 
 import rospy
-from sensor_msgs.msg import CompressedImage, Image, CameraInfo
+from sensor_msgs.msg import CompressedImage, Image
 from sensor_msgs.msg import Range
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
+
+def draw_dot_on_image(cv_image, range_value:float, color=(0, 0, 255)):
+
+    # Draw a red dot at the center of the image
+    height, width, _ = cv_image.shape
+    center = (int((width / 2)), int((height / 2)))
+    center_off = (center[0] + 6, center[1] - 19)
+
+    cv2.circle(cv_image, center_off, 2, (0, 0, 255), -1)
+
+    # Add a caption with the TOF range value near the center of the image
+    caption = f"{range_value:.2f}m"
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 0.4
+    color = (0, 0, 255)  # Red
+    thickness = 1
+    text_size, _ = cv2.getTextSize(caption, font, font_scale, thickness)
+    text_x = center_off[0] - text_size[0] // 2
+    text_y = center_off[1] - text_size[1] +5  # Slightly above the circle
+
+    cv2.putText(
+        cv_image,
+        caption,
+        (text_x, text_y),
+        font,
+        font_scale,
+        color,
+        thickness,
+    )
 
 
 class TofOnImage:
@@ -76,32 +105,7 @@ class TofOnImage:
         except CvBridgeError as e:
             rospy.logerr(e)
 
-        # Draw a red dot at the center of the image
-        height, width, _ = cv_image.shape
-        center = (int((width / 2)), int((height / 2)))
-        center_off = (center[0] + 6, center[1] - 19)
-
-        cv2.circle(cv_image, center_off, 2, (0, 0, 255), -1)
-
-        # Add a caption with the TOF range value near the center of the image
-        caption = f"{self.range.range:.2f} m"
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        font_scale = 0.5
-        color = (0, 0, 255)  # Red
-        thickness = 1
-        text_size, _ = cv2.getTextSize(caption, font, font_scale, thickness)
-        text_x = center_off[0] - text_size[0] // 2
-        text_y = center_off[1] - text_size[1] - 10  # Slightly above the circle
-
-        cv2.putText(
-            cv_image,
-            caption,
-            (text_x, text_y),
-            font,
-            font_scale,
-            color,
-            thickness,
-        )
+        draw_dot_on_image(cv_image, self.range.range)
 
         try:
             self.out_image.data = self.bridge.cv2_to_compressed_imgmsg(
