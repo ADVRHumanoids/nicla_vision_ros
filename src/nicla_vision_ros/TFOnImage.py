c16d34be97d0e122d25bc3d9f116a3646af34700
Davide Torielli <toridebraus@gmail.com>
2025-08-25 14:52:47 +0200

    fixes and improvments

--- src/nicla_vision_ros/TFOnImage.py
+++ src/nicla_vision_ros/TFOnImage.py
@@ -25,7 +25,12 @@ def draw_tf_on_image(cv_image:np.ndarray, cam_T_target:TransformStamped, cam_inf
     target_origin = np.array([cam_T_target.transform.translation.x, 
                               cam_T_target.transform.translation.y, 
                               cam_T_target.transform.translation.z])
-    target_R = quaternion_matrix(cam_T_target.transform.rotation)[:3, :3]
+    target_R = quaternion_matrix(
+        [cam_T_target.transform.rotation.x,
+        cam_T_target.transform.rotation.y,
+        cam_T_target.transform.rotation.z,
+        cam_T_target.transform.rotation.w] 
+    )[:3, :3]
 
     x_axis_cam = target_R.dot(np.array([1, 0, 0]))
     y_axis_cam = target_R.dot(np.array([0, 1, 0]))
@@ -44,11 +49,11 @@ def draw_tf_on_image(cv_image:np.ndarray, cam_T_target:TransformStamped, cam_inf
     if (0 <= p0[0] < cv_image.shape[1]) and (0 <= p0[1] < cv_image.shape[0]):
         cv2.circle(cv_image, p0, 6, (0, 0, 0), -1)  # black dot
     else:
-        rospy.logwarn("Origin out of bounds: %s", p0)
+        rospy.logwarn("Origin out of bounds: %s, from 3D %s", p0, target_origin)
     
-    cv2.line(cv_image, p0, px_end, (255, 0, 0), 4)  # Red X-axis
-    cv2.line(cv_image, p0, py_end, (0, 255, 0), 4)  # Green Y-axis
-    cv2.line(cv_image, p0, pz_end, (0, 0, 255), 4)  # Blue Z-axis
+    cv2.line(cv_image, p0, px_end, (0, 0, 255), 4)  # Red X-axis, color in BGR
+    cv2.line(cv_image, p0, py_end, (0, 255, 0), 4)  # Green Y-axis color in BGR
+    cv2.line(cv_image, p0, pz_end, (255, 0, 0), 4)  # Blue Z-axis color in BGR
 
     caption = cam_T_target.child_frame_id
     font = cv2.FONT_HERSHEY_SIMPLEX
@@ -59,13 +64,20 @@ def draw_tf_on_image(cv_image:np.ndarray, cam_T_target:TransformStamped, cam_inf
     text_x = p0[0] - text_size[0] // 2
     text_y = p0[1] - text_size[1] +5  # Slightly above the circle
 
+    cv2.putText(cv_image, "X", (px_end[0] + 5, px_end[1] - 5),
+                font, font_scale, (0, 0, 255), thickness)
+    cv2.putText(cv_image, "Y", (py_end[0] + 5, py_end[1] - 5),
+                font, font_scale, (0, 255, 0), thickness)
+    cv2.putText(cv_image, "Z", (pz_end[0] + 5, pz_end[1] - 5),
+                font, font_scale, (255, 0, 0), thickness)
+
     cv2.putText(
         cv_image,
         caption,
         (text_x, text_y),
         font,
-        font_scale,
-        color,
+        0.6,
+        (0, 0, 0),
         thickness,
     )
 
@@ -92,7 +104,7 @@ class TFOnImage:
 
         self.target_frames = rospy.get_param(
             "~target_frames",
-            ["dagana_1_tcp"],
+            ["target"],
         )
 
         if self.cam_compressed:
@@ -118,6 +130,7 @@ class TFOnImage:
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer)
 
+        self.cv_image = np.array([])
         self.out_image = CompressedImage()
 
         self.cam_T_targets = []
@@ -137,6 +150,19 @@ class TFOnImage:
     def range_callback(self, data):
         self.range = data
 
+    def get_image(self):
+        try:
+            if self.cam_compressed:
+                self.cv_image = self.bridge.compressed_imgmsg_to_cv2(
+                    self.image, "passthrough"
+                )
+            else:
+                self.cv_image = self.bridge.imgmsg_to_cv2(self.image, "bgr8")
+        except CvBridgeError as e:
+            rospy.logerr(e)
+            return False   
+        return True
+    
     def get_transforms(self):
         try:
             for i, target_frame in enumerate(self.target_frames):
@@ -146,34 +172,33 @@ class TFOnImage:
                     rospy.Time())
         except (LookupException, ConnectivityException, ExtrapolationException) as e:
             rospy.logerr("Error getting transforms: %s", e)
+            return False
+        
+        return True
                 
 
     def run(self):
 
         if not self.img_arrived:
-            return
+            return False
 
-        try:
-            if self.cam_compressed:
-                cv_image = self.bridge.compressed_imgmsg_to_cv2(
-                    self.image, "passthrough"
-                )
-            else:
-                cv_image = self.bridge.imgmsg_to_cv2(self.image, "bgr8")
-        except CvBridgeError as e:
-            rospy.logerr(e)
-
-        self.get_transforms()
+        if not self.get_image():
+            return False
+      
+        if not self.get_transforms():
+            return False
+        
         for cam_T_target in self.cam_T_targets:
-            draw_tf_on_image(cv_image, cam_T_target, self.cam_info)
+            draw_tf_on_image(self.cv_image, cam_T_target, self.cam_info)
 
         try:
             self.out_image.data = self.bridge.cv2_to_compressed_imgmsg(
-                cv_image, dst_format="jpg"
+                self.cv_image, dst_format="jpg"
             ).data
         except CvBridgeError as e:
             rospy.logerr(e)
+            return False
 
         self.img_pub.publish(self.out_image)
 
-        pass
+        return True
